d9c65872091f9bf97f109e93b9b5ffa09b3a1d5e
Mayank <mayankmallick42@gmail.com>
2021-07-28 20:27:57 +0530

    format doc

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-(function($) {
+(function ($) {
 
 	"use strict";
 
 	/* ----------------------------------------------------------- */
-	/*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
-    /* ----------------------------------------------------------- */
+	/*  FUNCTION TO STOP LOCAL SLIDESHOW
+	/* ----------------------------------------------------------- */
 
 	function stop_videos() {
 		var video = document.getElementById("video");
@@ -14,11 +14,11 @@
 		$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
 	}
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 
 		/* ----------------------------------------------------------- */
 		/*  STOP VIDEOS
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		$('.slideshow nav span').on('click', function () {
 			stop_videos();
@@ -26,25 +26,25 @@
 
 		/* ----------------------------------------------------------- */
 		/*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		$(".revealator-delay1").addClass('no-transform');
 
 		/* ----------------------------------------------------------- */
 		/*  PORTFOLIO GALLERY
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		if ($('.grid').length) {
-			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+			new CBPGridGallery(document.getElementById('grid-gallery'));
 		}
 
 		/* ----------------------------------------------------------- */
 		/*  BUTTONS ANIMATION
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 		function checkSize() {
-			if ($( document ).width() > 992) {
+			if ($(document).width() > 992) {
 				var btn_hover = "";
-				$(".btn").each(function() {
+				$(".btn").each(function () {
 					var btn_text = $(this).text();
 					$(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
 				});
@@ -57,25 +57,25 @@
 
 		/* ----------------------------------------------------------- */
 		/*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".grid figure").on('click', function() {
+		$(".grid figure").on('click', function () {
 			$("#navbar-collapse-toggle").addClass('hide-header');
 		});
 
 		/* ----------------------------------------------------------- */
 		/*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".nav-close").on('click', function() {
+		$(".nav-close").on('click', function () {
 			$("#navbar-collapse-toggle").removeClass('hide-header');
 		});
-		$(".nav-prev").on('click', function() {
+		$(".nav-prev").on('click', function () {
 			if ($('.slideshow ul li:first-child').hasClass('current')) {
 				$("#navbar-collapse-toggle").removeClass('hide-header');
 			}
 		});
-		$(".nav-next").on('click', function() {
+		$(".nav-next").on('click', function () {
 			if ($('.slideshow ul li:last-child').hasClass('current')) {
 				$("#navbar-collapse-toggle").removeClass('hide-header');
 			}
@@ -83,7 +83,7 @@
 
 		/* ----------------------------------------------------------- */
 		/*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		var item = $(".grid li figure");
 		var elementsLength = item.length;
@@ -93,9 +93,9 @@
 
 		/* ----------------------------------------------------------- */
 		/*  AJAX CONTACT FORM
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".contactform").on("submit", function() {
+		$(".contactform").on("submit", function () {
 			$(".output_message").text("Sending...");
 
 			var form = $(this);
@@ -103,7 +103,7 @@
 				url: form.attr("action"),
 				method: form.attr("method"),
 				data: form.serialize(),
-				success: function(result) {
+				success: function (result) {
 					if (result == "success") {
 						$(".form-inputs").css("display", "none");
 						$(".box p").css("display", "none");
@@ -123,11 +123,11 @@
 
 	});
 
-	$(document).keyup(function(e) {
+	$(document).keyup(function (e) {
 
 		/* ----------------------------------------------------------- */
 		/*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 		if (e.keyCode === 27) {
 			stop_videos();
 			$('.close-content').click();
--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,49 +1,49 @@
 /**	STYLE SWITCHER
 *************************************************** **/
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 	"use strict";
 
-    jQuery("#hideSwitcher, #showSwitcher").click(function () {
+	jQuery("#hideSwitcher, #showSwitcher").click(function () {
 
-        if (jQuery("#showSwitcher").is(":visible")) {
+		if (jQuery("#showSwitcher").is(":visible")) {
 
 			var _identifier = "#showSwitcher";
-            jQuery("#switcher").animate({"margin-left": "0px"}, 500).show();
+			jQuery("#switcher").animate({ "margin-left": "0px" }, 500).show();
 			createCookie("switcher_visible", 'true', 365);
 
-        } else {
+		} else {
 
 			var _identifier = "#switcher";
-            jQuery("#showSwitcher").show().animate({"margin-left": "0"}, 500);
+			jQuery("#showSwitcher").show().animate({ "margin-left": "0" }, 500);
 			createCookie("switcher_visible", 'false', 365);
 
-        }
+		}
 
-		jQuery(_identifier).animate({"margin-left": "-500px"}, 500, function () {
+		jQuery(_identifier).animate({ "margin-left": "-500px" }, 500, function () {
 			jQuery(this).hide();
 		});
 
-    });
+	});
 
 	// REMOVE # FROM URL
-	$("a[href='#']").on("click", (function(e) {
+	$("a[href='#']").on("click", (function (e) {
 		e.preventDefault();
 	}));
-                      
+
 });
 
 function setActiveStyleSheet(title) {
 	var i, a, main;
-	for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-		if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+	for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
+		if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
 			a.disabled = true;
-			if(a.getAttribute("title") == title) { a.disabled = false; }
+			if (a.getAttribute("title") == title) { a.disabled = false; }
 		}
 	}
 
 	// DARK SKIN
 	var color_skin = readCookie('color_skin');
-	if(color_skin == 'dark') {
+	if (color_skin == 'dark') {
 		jQuery("#css_dark_skin").remove();
 		jQuery("head").append('<link id="css_dark_skin" href="assets/css/layout-dark.css" rel="stylesheet" type="text/css" title="dark" />');
 		jQuery("#is_dark").trigger('click');
@@ -53,8 +53,8 @@ function setActiveStyleSheet(title) {
 
 function getActiveStyleSheet() {
 	var i, a;
-	for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-		if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) { return a.getAttribute("title"); }
+	for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
+		if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) { return a.getAttribute("title"); }
 	}
 
 	return null;
@@ -62,8 +62,8 @@ function getActiveStyleSheet() {
 
 function getPreferredStyleSheet() {
 	var i, a;
-	for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-		if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("rel").indexOf("alt") == -1 && a.getAttribute("title")) {
+	for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
+		if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("rel").indexOf("alt") == -1 && a.getAttribute("title")) {
 			return a.getAttribute("title");
 		}
 	}
@@ -71,28 +71,28 @@ function getPreferredStyleSheet() {
 	return null;
 }
 
-function createCookie(name,value,days) {
+function createCookie(name, value, days) {
 	if (days) {
 		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
 	} else {
 		expires = "";
-	}	document.cookie = name+"="+value+expires+";";
+	} document.cookie = name + "=" + value + expires + ";";
 }
 
 function readCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
+	for (var i = 0; i < ca.length; i++) {
 		var c = ca[i];
 
-		while (c.charAt(0)==' ') {
-			c = c.substring(1,c.length);
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1, c.length);
 		}
 
 		if (c.indexOf(nameEQ) == 0) {
-			return c.substring(nameEQ.length,c.length);
+			return c.substring(nameEQ.length, c.length);
 		}
 	}
 
@@ -108,7 +108,7 @@ function readCookie(name) {
 	@ON LOAD
 **/
 var switcher_visible = '';
-window.onload = function(e) {
+window.onload = function (e) {
 
 	// COLOR SCHEME
 	var cookie = readCookie("style");
@@ -116,13 +116,13 @@ window.onload = function(e) {
 	setActiveStyleSheet(title);
 
 	// SWITCHER OPEN|CLOSED
-	if(switcher_visible != 'false') {
+	if (switcher_visible != 'false') {
 		jQuery("#showSwitcher").trigger('click');
 	}
 
 	// DARK OR LIGHT
 	var is_dark = readCookie('is_light');
-	if(is_boxed == 'true') {
+	if (is_boxed == 'true') {
 		jQuery('light').removeClass('light');
 		jQuery('light').addClass('light');
 		jQuery("#is_light").trigger('click');
@@ -134,10 +134,10 @@ window.onload = function(e) {
 /**
 	COLOR SKIN [light|dark]
 **/
-jQuery("input.dark_switch").bind("click", function() {
+jQuery("input.dark_switch").bind("click", function () {
 	var boxed_switch = jQuery(this).attr('value');
 
-	if(boxed_switch == 'light') {
+	if (boxed_switch == 'light') {
 		jQuery("body").removeClass('dark');
 		jQuery("body").addClass('light');
 	} else {
@@ -154,10 +154,10 @@ jQuery("input.dark_switch").bind("click", function() {
 /**
 	LAYOUT STYLE [wide|boxed]
 **/
-jQuery("input.boxed_switch").bind("click", function() {
+jQuery("input.boxed_switch").bind("click", function () {
 	var boxed_switch = jQuery(this).attr('value');
 
-	if(boxed_switch == 'boxed') {
+	if (boxed_switch == 'boxed') {
 		jQuery("body").removeClass('boxed');
 		jQuery("body").addClass('boxed');
 		createCookie("is_boxed", 'true', 365);
@@ -173,41 +173,41 @@ jQuery("input.boxed_switch").bind("click", function() {
 /**
 	SEPARATOR STYLE [Normal|Skew|Reversed Skew|Double Diagonal|Big Triangle]
 **/
-jQuery("input.separator_switch").bind("click", function() {
+jQuery("input.separator_switch").bind("click", function () {
 	var separator_switch = jQuery(this).attr('value');
 
-	if(separator_switch == 'skew') {
+	if (separator_switch == 'skew') {
 		jQuery("body").removeClass('reversed-skew');
 		jQuery("body").removeClass('double-diagonal');
 		jQuery("body").removeClass('big-triangle');
 		jQuery("body").addClass('skew');
 		createCookie("is_skew", 'true', 365);
 	}
-	
-	else if(separator_switch == 'reversed-skew') {
+
+	else if (separator_switch == 'reversed-skew') {
 		jQuery("body").removeClass('skew');
 		jQuery("body").removeClass('double-diagonal');
 		jQuery("body").removeClass('big-triangle');
 		jQuery("body").addClass('reversed-skew');
 		createCookie("is_reversed_skew", 'true', 365);
 	}
-	
-	else if(separator_switch == 'double-diagonal') {
+
+	else if (separator_switch == 'double-diagonal') {
 		jQuery("body").removeClass('skew');
 		jQuery("body").removeClass('reversed-skew');
 		jQuery("body").removeClass('big-triangle');
 		jQuery("body").addClass('double-diagonal');
 		createCookie("is_double_diagonal", 'true', 365);
 	}
-	
-	else if(separator_switch == 'big-triangle') {
+
+	else if (separator_switch == 'big-triangle') {
 		jQuery("body").removeClass('skew');
 		jQuery("body").removeClass('reversed-skew');
 		jQuery("body").removeClass('double-diagonal');
 		jQuery("body").addClass('big-triangle');
 		createCookie("is_big_triangle", 'true', 365);
 	}
-	
+
 	else {
 		jQuery("body").removeClass('skew');
 		jQuery("body").removeClass('reversed-skew');
